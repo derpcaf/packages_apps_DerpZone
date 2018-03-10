bafb92c4cbc532d3eac1295ac1cc77e1504fe514
maxwen <max.weninger@gmail.com>
2018-03-10 05:10:16 +0000

    [2/2] DerpZone: featureize ro.recents.grid and Go Layout
    thanks google for coding like a 1st grader by adding public
    member variables and making this more work then needed
    
    Change-Id: Ia21792972dd7913d5c3f647d95ac79edb00096dd

--- src/com/derpcaf/derpzone/fragments/RecentsSettings.java
+++ src/com/derpcaf/derpzone/fragments/RecentsSettings.java
@@ -28,11 +28,15 @@ public class RecentsSettings extends SettingsPreferenceFragment implements
     private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
     private static final String IMMERSIVE_RECENTS = "immersive_recents";
     private static final String RECENTS_COMPONENT_TYPE = "recents_component";
+    private static final String RECENTS_TYPE = "recents_layout_style";
+    private static final int RECENTS_COMPONENT_OREO = 1;
 
     private ListPreference mRecentsComponentType;
     private ListPreference mImmersiveRecents;
     private ListPreference mRecentsClearAllLocation;
     private SwitchPreference mRecentsClearAll;
+    private ListPreference mRecentsType;
+    private PreferenceCategory oreoRecentsCat;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -49,6 +53,16 @@ public class RecentsSettings extends SettingsPreferenceFragment implements
         mRecentsClearAllLocation.setValue(String.valueOf(location));
         mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntry());
         mRecentsClearAllLocation.setOnPreferenceChangeListener(this);
+
+        // oreo recents type
+        mRecentsType = (ListPreference) findPreference(RECENTS_TYPE);
+        int style = Settings.System.getIntForUser(resolver,
+                Settings.System.RECENTS_LAYOUT_STYLE, 0, UserHandle.USER_CURRENT);
+        mRecentsType.setValue(String.valueOf(style));
+        mRecentsType.setSummary(mRecentsType.getEntry());
+        mRecentsType.setOnPreferenceChangeListener(this);
+        oreoRecentsCat = (PreferenceCategory)findPreference("recents_ui_oreo_recents_category");
+        oreoRecentsCat.setEnabled(type == RECENTS_COMPONENT_OREO);
 
         mImmersiveRecents = (ListPreference) findPreference(IMMERSIVE_RECENTS);
         mImmersiveRecents.setValue(String.valueOf(Settings.System.getInt(
@@ -81,13 +95,22 @@ public class RecentsSettings extends SettingsPreferenceFragment implements
             mImmersiveRecents.setValue(String.valueOf(objValue));
             mImmersiveRecents.setSummary(mImmersiveRecents.getEntry());
             return true;
+        } else if (preference == mRecentsType) {
+            int style = Integer.valueOf((String) objValue);
+            int index = mRecentsType.findIndexOfValue((String) objValue);
+            Settings.System.putIntForUser(getActivity().getContentResolver(),
+                    Settings.System.RECENTS_LAYOUT_STYLE, style, UserHandle.USER_CURRENT);
+            mRecentsType.setSummary(mRecentsType.getEntries()[index]);
+            DerpcafUtils.showSystemUiRestartDialog(getContext());
+        return true;
         } else if (preference == mRecentsComponentType) {
             int type = Integer.valueOf((String) objValue);
             int index = mRecentsComponentType.findIndexOfValue((String) objValue);
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.RECENTS_COMPONENT, type);
             mRecentsComponentType.setSummary(mRecentsComponentType.getEntries()[index]);
-            if (type == 1) { // Disable swipe up gesture, if oreo type selected
+            oreoRecentsCat.setEnabled(type == RECENTS_COMPONENT_OREO);
+            if (type == RECENTS_COMPONENT_OREO) { // Disable swipe up gesture, if oreo type selected
                Settings.Secure.putInt(getActivity().getContentResolver(),
                     Settings.Secure.SWIPE_UP_TO_SWITCH_APPS_ENABLED, 0);
             }
