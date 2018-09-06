d8620445566c48b2e8e59dff2f87cfc3ad4801f3
beanstown106 <nbenis106@gmail.com>
2018-09-06 22:52:55 +0530

    incall vibration options [3/3] *allow setting vibration when call is connected *allow setting vibration when call is disconnected *allow setting vibration for call waiting *this works with google and aosp dialer :) SuperDroidBond-: Make it default on

--- src/com/derpcaf/derpzone/fragments/NotificationSettings.java
+++ src/com/derpcaf/derpzone/fragments/NotificationSettings.java
@@ -1,6 +1,9 @@
 package com.derpcaf.derpzone.fragments;
 
 import com.android.internal.logging.nano.MetricsProto;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+import com.derpcaf.derpzone.preferences.DerpcafUtils;
 
 import android.os.Bundle;
 import com.android.settings.R;
@@ -9,11 +12,19 @@ import com.android.settings.SettingsPreferenceFragment;
 
 public class NotificationSettings extends SettingsPreferenceFragment {
 
+    private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.derpzone_notifications);
+
+        PreferenceScreen prefScreen = getPreferenceScreen();
+        PreferenceCategory incallVibCategory = (PreferenceCategory) findPreference(INCALL_VIB_OPTIONS);
+        if (!DerpcafUtils.isVoiceCapable(getActivity())) {
+            prefScreen.removePreference(incallVibCategory);
+        }
     }
 
     @Override
