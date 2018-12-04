afc0dc07a60ef6f03290536b050740328fedd71d
ZyxxeZ <bankersenator@gmail.com>
2018-12-04 19:06:53 +0000

    Revert "Unlock keystore with fingerprint after reboot (2/2)"
    This reverts commit 38721e8f9beae8a4533fad838de8861dd349a764.

--- src/com/derpcaf/derpzone/fragments/LockScreenSettings.java
+++ src/com/derpcaf/derpzone/fragments/LockScreenSettings.java
@@ -39,9 +39,6 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
 import com.derpcaf.derpzone.preferences.DerpcafUtils;
-import com.derpcaf.derpzone.preferences.CustomSeekBarPreference;
-import com.derpcaf.derpzone.preferences.SystemSettingSwitchPreference;
-
 
 public class LockScreenSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
@@ -50,13 +47,11 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
     private static final String KEY_FACE_UNLOCK_PACKAGE = "com.android.facelock";
     private static final String FINGERPRINT_VIB = "fingerprint_success_vib";
     private static final String LOCK_CLOCK_FONTS = "lock_clock_fonts";
-    private static final String FP_UNLOCK_KEYSTORE = "fp_unlock_keystore";
 
     private FingerprintManager mFingerprintManager;
     private SwitchPreference mFingerprintVib;
     private SwitchPreference mFaceUnlock;
     ListPreference mLockClockFonts;
-    private SystemSettingSwitchPreference mFpKeystore;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -78,14 +73,12 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
 
         mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
         mFingerprintVib = (SwitchPreference) findPreference(FINGERPRINT_VIB);
-        mFpKeystore = (SystemSettingSwitchPreference) findPreference(FP_UNLOCK_KEYSTORE);
-        if (mFingerprintManager == null){
+        if (!mFingerprintManager.isHardwareDetected()){
             prefScreen.removePreference(mFingerprintVib);
-            prefScreen.removePreference(mFpKeystore);
         } else {
-            mFingerprintVib.setChecked((Settings.System.getInt(getContentResolver(),
-                    Settings.System.FINGERPRINT_SUCCESS_VIB, 1) == 1));
-            mFingerprintVib.setOnPreferenceChangeListener(this);
+        mFingerprintVib.setChecked((Settings.System.getInt(getContentResolver(),
+                Settings.System.FINGERPRINT_SUCCESS_VIB, 1) == 1));
+        mFingerprintVib.setOnPreferenceChangeListener(this);
         }
 
         // Lockscren Clock Fonts
